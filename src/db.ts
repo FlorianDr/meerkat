24c85cc99f65361ca92b3d2e398de740668bed0d
Florian Dreschner <code@dreschner.eu>
2024-08-10 16:21:35 +0200

    fix: Increase timeout and use prepare

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,11 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import env from "./env.ts";
 
-const client = postgres(env.connectionString);
+const client = postgres(env.connectionString, {
+  // Disable prefetch as suggested by https://supabase.com/docs/guides/database/connecting-to-postgres#connecting-with-drizzle
+  prepare: false,
+  connect_timeout: 30,
+});
 
 const db = drizzle(client);
 
