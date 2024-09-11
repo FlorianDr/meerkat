a5d3f63a879feb804faddce8e22f89e049775a4d
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-09-11 15:32:04 +0200

    refactor: removed unused query

--- api/models/votes.ts
+++ api/models/votes.ts
@@ -1,25 +1,6 @@
-import { and, eq, sql } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import { votes } from "../schema.ts";
 import db from "../db.ts";
-
-const voteByQuestionIdandUserId = db.select().from(votes).where(
-  and(
-    eq(votes.questionId, sql.placeholder("question_id")),
-    eq(votes.userId, sql.placeholder("user_id")),
-  ),
-).prepare("vote_by_question_id_and_user_id");
-
-export async function getVoteByQuestionIdandUserId(
-  questionId: number,
-  userId: number,
-): Promise<Vote[]> {
-  const results = await voteByQuestionIdandUserId.execute({
-    question_id: questionId, // Corrected placeholder name
-    user_id: userId, // Corrected placeholder name
-  });
-
-  return results;
-}
 
 export async function addVote(
   questionId: number,
