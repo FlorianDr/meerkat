c1aa762eba3e83971cd9462283cfc3405d6a15b4
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-10-09 11:02:01 +0200

    feat: added a cooldown modal to submit question

--- ui/src/components/QnA/Footer.tsx
+++ ui/src/components/QnA/Footer.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
   Flex,
   Icon,
@@ -7,15 +8,15 @@ import {
   InputRightElement,
   useToast,
 } from "@chakra-ui/react";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { User } from "../../types.ts";
-import { Event } from "../../hooks/use-event.ts";
-import { useThemeColors } from "../../hooks/use-theme-colors.ts";
-import { PrimaryButton } from "../Buttons/PrimaryButton.tsx";
-import { HeartIcon } from "./HeartIcon.tsx";
-import { useReact } from "../../hooks/use-react.ts";
-import { useLogin } from "../../hooks/use-login.ts";
 import { useAsyncFormSubmit } from "../../hooks/use-async-form-submit.ts";
+import { Event } from "../../hooks/use-event.ts";
+import { useLogin } from "../../hooks/use-login.ts";
+import { useReact } from "../../hooks/use-react.ts";
+import { useThemeColors } from "../../hooks/use-theme-colors.ts";
+import { User } from "../../types.ts";
+import { PrimaryButton } from "../Buttons/PrimaryButton.tsx";
+import { Modal } from "../Modal/Modal.tsx";
+import { HeartIcon } from "./HeartIcon.tsx";
 
 export type FooterProps = {
   event: Event | undefined;
@@ -34,7 +35,7 @@ export function Footer({
   const { trigger } = useReact(event?.uid ?? "");
   const { login, isLoading } = useLogin();
   const toast = useToast();
-  const { onSubmit } = useAsyncFormSubmit({
+  const { onSubmit, error } = useAsyncFormSubmit({
     onSuccess: () => {
       toast({
         title: "Question added 🎉",
@@ -48,82 +49,96 @@ export function Footer({
   const action = `/api/v1/events/${event?.uid}/questions`;
 
   return (
-    <div className="overlay-container">
-      <form
-        className="target question-input"
-        onSubmit={onSubmit}
-        method="POST"
-        action={action}
-      >
-        <Flex gap={2} flexFlow="row" alignItems="center">
-          <InputGroup size="lg">
-            <Input
-              size="lg"
-              disabled={!isAuthenticated}
-              placeholder={!isAuthenticated
-                ? "Please, login before submitting a question!"
-                : "Type a question..."}
-              name="question"
-              bg="#342749"
-              color="white"
-              borderColor={primaryPurple}
-              border="none"
-              borderRadius="md"
-              p={4}
-              _placeholder={{ color: "white" }}
-            />
-            <InputRightElement width="auto" pr={2}>
-              <IconButton
-                isDisabled={!isAuthenticated}
-                type="submit"
-                h="1.75rem"
-                size="sm"
-                colorScheme="purple"
-                icon={<SendIcon />}
-                _hover={isAuthenticated
-                  ? {
-                    bg: primaryPurple,
-                    color: "white",
-                    opacity: 0.8,
-                  }
-                  : {}}
-                aria-label="Submit question"
+    <>
+      <div className="overlay-container">
+        <form
+          className="target question-input"
+          onSubmit={onSubmit}
+          method="POST"
+          action={action}
+        >
+          <Flex gap={2} flexFlow="row" alignItems="center">
+            <InputGroup size="lg">
+              <Input
+                size="lg"
+                disabled={!isAuthenticated}
+                placeholder={!isAuthenticated
+                  ? "Please, login before submitting a question!"
+                  : "Type a question..."}
+                name="question"
+                bg="#342749"
+                color="white"
+                borderColor={primaryPurple}
+                border="none"
+                borderRadius="md"
+                p={4}
+                _placeholder={{ color: "white" }}
               />
-            </InputRightElement>
-          </InputGroup>
-          <IconButton
-            isDisabled={!isAuthenticated}
-            onClick={() => trigger()}
-            size="lg"
-            colorScheme="purple"
-            bg="#342749"
-            icon={<HeartIcon />}
-            _hover={isAuthenticated
-              ? {
-                bg: primaryPurple,
-                color: "white",
-                opacity: 0.8,
-              }
-              : {}}
-            aria-label="React to event"
-          />
-        </Flex>
-        <span className="signin-name">
-          Signed as {user?.name ?? user?.uid ?? "Anonymous"}
-        </span>
-      </form>
-      {!isAuthenticated && (
-        <LoginOverlay>
-          <PrimaryButton
-            isLoading={isLoading}
-            loadingText="Connecting..."
-            onClick={() => login()}
-          >
-            Login with Zupass <ExternalLinkIcon />
-          </PrimaryButton>
-        </LoginOverlay>
+              <InputRightElement width="auto" pr={2}>
+                <IconButton
+                  isDisabled={!isAuthenticated}
+                  type="submit"
+                  h="1.75rem"
+                  size="sm"
+                  colorScheme="purple"
+                  icon={<SendIcon />}
+                  _hover={isAuthenticated
+                    ? {
+                      bg: primaryPurple,
+                      color: "white",
+                      opacity: 0.8,
+                    }
+                    : {}}
+                  aria-label="Submit question"
+                />
+              </InputRightElement>
+            </InputGroup>
+            <IconButton
+              isDisabled={!isAuthenticated}
+              onClick={() => trigger()}
+              size="lg"
+              colorScheme="purple"
+              bg="#342749"
+              icon={<HeartIcon />}
+              _hover={isAuthenticated
+                ? {
+                  bg: primaryPurple,
+                  color: "white",
+                  opacity: 0.8,
+                }
+                : {}}
+              aria-label="React to event"
+            />
+          </Flex>
+          <span className="signin-name">
+            Signed as {user?.name ?? user?.uid ?? "Anonymous"}
+          </span>
+        </form>
+        {!isAuthenticated && (
+          <LoginOverlay>
+            <PrimaryButton
+              isLoading={isLoading}
+              loadingText="Connecting..."
+              onClick={() => login()}
+            >
+              Login with Zupass <ExternalLinkIcon />
+            </PrimaryButton>
+          </LoginOverlay>
+        )}
+      </div>
+      {error?.cause === "cooldown" && (
+        <Modal
+          isOpen={true}
+          onClose={() => {}}
+          title="Cooldown"
+          lockFocusAcrossFrames
+        >
+          <p>
+            You are on cooldown. Please, try again later.
+          </p>
+        </Modal>
       )}
-    </div>
+    </>
   );
 }
 
--- ui/src/hooks/use-async-form-submit.ts
+++ ui/src/hooks/use-async-form-submit.ts
@@ -4,11 +4,17 @@ export type AsyncFormSubmitProps = {
   onSuccess?: () => void;
 };
 
+type AsyncFormSubmitReturnType = {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+  isLoading: boolean;
+  error: Error | null;
+};
+
 export const useAsyncFormSubmit = (
   props?: AsyncFormSubmitProps | undefined,
-) => {
+): AsyncFormSubmitReturnType => {
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<unknown>(null);
+  const [error, setError] = useState<Error | null>(null);
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -22,6 +28,12 @@ export const useAsyncFormSubmit = (
         method,
         body: formData,
       });
+
+      if (response.status === 403) {
+        throw new Error(`${response.status} - ${await response.text()}`, {
+          cause: "cooldown",
+        });
+      }
 
       if (!response.ok) {
         throw new Error(`${response.status} - ${await response.text()}`);
