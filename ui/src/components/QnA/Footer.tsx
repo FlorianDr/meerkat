a27e85c9feaf86db9a119dfa05ec430008f6fc06
Florian Dreschner <code@dreschner.eu>
2024-10-27 20:02:04 +0100

    fix: stop button from jumping away

--- ui/src/components/QnA/Footer.tsx
+++ ui/src/components/QnA/Footer.tsx
@@ -1,22 +1,12 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import {
-  Flex,
-  Icon,
-  IconButton,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Textarea,
-  useToast,
-} from "@chakra-ui/react";
-import { useAsyncFormSubmit } from "../../hooks/use-async-form-submit.ts";
+import { Flex, Icon, IconButton, Textarea, useToast } from "@chakra-ui/react";
 import { useLogin } from "../../hooks/use-login.ts";
 import { useThemeColors } from "../../hooks/use-theme-colors.ts";
 import { User } from "../../types.ts";
 import { PrimaryButton } from "../Buttons/PrimaryButton.tsx";
 import { HeartIcon } from "./HeartIcon.tsx";
 import { Event } from "../../types.ts";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useAskQuestion } from "../../hooks/use-ask-question.ts";
 
 const MAX_QUESTION_LENGTH = 200;
@@ -53,6 +43,16 @@ export function Footer({
     },
   });
 
+  const submitQuestion = () => {
+    if (question) {
+      trigger({
+        question,
+      });
+    }
+  };
+
+  const isQuestionMode = focused || question;
+
   return (
     <>
       <div className="overlay-container">
@@ -63,13 +63,11 @@ export function Footer({
               size="lg"
               minH="48px"
               value={question}
-              onChange={(e) => setQuestion(e.target.value)}
+              onChange={(e) => setQuestion(e.target.value.trim())}
               onKeyDown={(e) => {
-                if (e.key === "Enter" && question) {
+                if (e.key === "Enter" && !e.shiftKey) {
                   e.preventDefault();
-                  trigger({
-                    question,
-                  });
+                  submitQuestion();
                 }
               }}
               disabled={!isAuthenticated}
@@ -79,7 +77,7 @@ export function Footer({
               color="white"
               onFocus={() => setFocused(true)}
               onBlur={() => setFocused(false)}
-              rows={focused ? 3 : 1}
+              rows={isQuestionMode ? 3 : 1}
               borderColor={primaryPurple}
               border="none"
               borderRadius="md"
@@ -90,17 +88,13 @@ export function Footer({
             <IconButton
               isDisabled={!isAuthenticated}
               size="lg"
-              onClick={() => {
-                trigger({
-                  question,
-                });
-              }}
+              onClick={submitQuestion}
               bg="purple.500"
               _hover={{ bg: "purple.600" }}
               icon={<SendIcon />}
               aria-label="Submit question"
             />
-            {!focused
+            {!isQuestionMode
               ? (
                 <IconButton
                   isDisabled={!isAuthenticated}
