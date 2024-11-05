b6555acf88d7dae7fafe271d51e145daa669e20f
Florian Dreschner <code@dreschner.eu>
2024-11-05 13:08:16 +0700

    fix: stop revalidation on focus

--- ui/src/hooks/use-event.ts
+++ ui/src/hooks/use-event.ts
@@ -7,6 +7,9 @@ export const useEvent = (uid: string | undefined) => {
   const { data, error, isLoading, mutate } = useSWR<{ data: Event }, HTTPError>(
     uid ? `/api/v1/events/${uid}` : undefined,
     fetcher,
+    {
+      revalidateOnFocus: false,
+    },
   );
 
   return { data: data?.data, error, isLoading, mutate };
--- ui/src/hooks/use-user.ts
+++ ui/src/hooks/use-user.ts
@@ -11,6 +11,7 @@ export const useUser = () => {
     "/api/v1/users/me",
     fetcher,
     {
+      revalidateOnFocus: false,
       onSuccess: (data) => {
         setUser(data.data);
       },
