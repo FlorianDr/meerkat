03d3caa7a8ede6d9fc48ecd02b7b8e6de1e9b4f2
Florian Dreschner <code@dreschner.eu>
2024-11-10 14:14:36 +0700

    infra: set db max pool size in env

--- api/db.ts
+++ api/db.ts
@@ -2,11 +2,13 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import env from "./env.ts";
 
+console.info(`DB - maxPoolSize: ${env.maxPoolSize}`);
+
 const client = postgres(env.connectionString, {
   // Disable prefetch as suggested by https://supabase.com/docs/guides/database/connecting-to-postgres#connecting-with-drizzle
   prepare: false,
   connect_timeout: 30,
-  // Default is 10 connections
+  // Default is 10 connections, tuning with https://supabase.com/docs/guides/platform/compute-and-disk#postgres-replication-slots-wal-senders-and-connections
   max: Number(env.maxPoolSize ?? "10"),
 });
 
