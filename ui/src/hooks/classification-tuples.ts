0c434a680649aef2fa937d3b4b226b8de204e302
Florian Dreschner <code@dreschner.eu>
2024-11-11 11:31:38 +0700

    feat: add ticket list

--- ui/src/hooks/classification-tuples.ts
+++ ui/src/hooks/classification-tuples.ts
@@ -4,3 +4,153 @@ export const classificationTuples = [
     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
   },
 ];
+
+// Ideally, we would have a list of productIds to be excluded from the classification tuples
+// but there is no API for it. The list below
+// export const classificationTuples = [
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     productId: "220f81f4-ca7b-4e47-bfb7-14bf1aa94a89", // General Admission
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "45b07aad-b4cf-4f0e-861b-683ba3de49bd", // Protocol Guild / Merge Pass Holders
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "e6df2335-00d5-4ee1-916c-977d326a9049", // OSS Contributors
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "2ab74a56-4182-4798-a485-6380f87d6299", // Solo Stakers
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "9fb49dd1-edea-4c57-9ff2-6e6c9c3b4a0a", // Public Goods Fundraisers
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "1ad9e110-8745-4eed-8ca5-ee5b8cd69c0f", // Raffle-Auction Winner
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "6b0f70f1-c757-40a1-b6ab-a9ddab221615", // Devcon 6 Attendees
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "08482abb-8767-47aa-be47-2691032403b6", // Complimentary Ticket
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "2a86d360-4ca2-43b5-aeb5-9a070da9a992", // Local SEA Builder Discount
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "c900d46a-99fd-4f7a-8d6b-10d041b2601b", // Builder Discont
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "3de2fcc5-3822-460c-8175-2eef211d2f1d", // Academic Discount
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "c97cb25e-302b-4696-ac24-2a7a8255572e", // Youth Discount
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "695cedfe-a973-4371-acc4-907bde4251c5", // Volunteer
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "41a055e0-db9c-41ff-8e9c-5834c9d64c6d", // Press
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "b50febf2-a258-4ee6-b3e4-2b2c2e57a74e", // Supporter
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "f15237ec-abd9-40ae-8e61-9cf8a7a60c3f", // EFer
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "a4c658af-0b37-41ac-aa0a-850b6b7741be", // EF Guests
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "c64cac28-5719-4260-bd9a-ea0c0cb04d54", // Speakers
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "bc67b24b-52e1-418e-832a-568d1ae5a58c", // POAP Holder Discount
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "b44cac2f-92b5-405e-9aa1-7127661790e2", // DAO Governance Participants
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "81620f49-d7fc-4ccb-a7bb-0ad81d97191a", // Indian Resident (AnonAadhaar) Builder
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "1bbc0ec1-5be9-43ff-acd3-d4ca794f814f", // zkPassport Local Builder
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "3c30c8e0-4f96-4b46-b2c9-72954e31ab51", // Reserve
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "f4f14100-f816-4e2e-a770-78dacaee4e2f", // Grant Recipients
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "5dcea12b-5862-404f-8943-2fbb35322e4e", // Community Reserve
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "c751e137-bb3c-44f3-94c2-f81f0bc00276", // Devcon Scholar
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "8725de16-8edc-4c13-aeaa-1819aee98aeb", // Day 2 Pass (Nov. 13)
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "7a88209f-3248-4a53-b568-bb6861e60506", // Day 3 Pass (Nov. 14)
+//   },
+//   {
+//     signerPublicKey: "YwahfUdUYehkGMaWh0+q3F8itx2h8mybjPmt8CmTJSs",
+//     eventId: "5074edf5-f079-4099-b036-22223c0c6995",
+//     "productId": "7d4f5da2-9d5f-46ab-a28f-7cf38e016281", // Day 4 Pass (Nov. 15)
+//   },
+// ];
