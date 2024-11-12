325fff6ece4db05ee3ecf333017c94cd52c1ddc4
Florian Dreschner <code@dreschner.eu>
2024-11-12 10:46:17 +0700

    fix: import cover track sync

--- api/devcon/tracks.ts
+++ api/devcon/tracks.ts
@@ -3,11 +3,12 @@ import { DEFAULT_COVER } from "../models/event.ts";
 const trackImages: Record<string, string> = {
   "Core Protocol":
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/elephant_Core%20Protocol.png?t=2024-10-31T11%3A19%3A34.065Z",
-  "Cypherpunk and Privacy":
+  "Cypherpunk & Privacy":
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/gecko_Cypherpunk%20and%20Privacy.png?t=2024-10-31T11%3A19%3A53.391Z",
   "Usability":
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/cat_Usability.png?t=2024-10-31T11%3A19%3A03.816Z",
-  "Real World Ethereum": "https://supabase.storage.url/track_2_image.jpg",
+  "Real World Ethereum":
+    "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/buffalo_Real%20World%20Ethereum.png?t=2024-11-06T05%3A17%3A07.833Z",
   "Applied Cryptography":
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/owl_Applied%20Cryptography.png?t=2024-10-31T11%3A20%3A31.354Z",
   "Cryptoeconomics":
@@ -18,7 +19,7 @@ const trackImages: Record<string, string> = {
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/turtle_Developer%20Experience.png?t=2024-10-31T11%3A20%3A53.145Z",
   "Security":
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/tiger_Security.png?t=2024-10-31T11%3A20%3A42.924Z",
-  "Layer 2s":
+  "Layer 2":
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/deer_Layer%202s.png?t=2024-10-31T11%3A19%3A20.962Z",
   "Programmable Cryptography":
     "https://icnyvghgspgzemdudsrd.supabase.co/storage/v1/object/public/images/frog_CLS_ProgrammableCryptography.png?t=2024-10-31T11%3A19%3A43.212Z",
